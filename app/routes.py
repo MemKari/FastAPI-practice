4e1b664e6a997800c7f19ff50c45cafb566b2a12
Mariia Zhuravlova <memkari666@gmail.com>
2024-05-25 18:11:00 +0400

    5 lesson добавлена возможность работать с файлами

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from models import User, Trade, Degree
 from databases import fake_users, fake_trades
 
@@ -10,6 +10,12 @@ router = APIRouter()
 @router.get('/get_user/{user_id}')
 async def getting_user(user_id: int):
     return [user for user in fake_users if user.get('id') == user_id][0]
+
+
+@router.get('/all_users')
+async def get_all_users(limit: int = 5):
+    users_in_limit = fake_users[:limit]
+    return users_in_limit
 
 
 @router.post('/new_account', response_model=User)
@@ -23,3 +29,13 @@ async def create_new_user(degree: Degree, id: int, name: str, role: str = 'trade
 async def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {'status': 200, 'result': 'Trades added successfully'}
+
+
+@router.post('/files')
+async def add_file(file: Annotated[bytes, File(description='to set metadata')]):  # File загружает только содержимое файла,
+    return {'File was processed. File size:': len(file)}
+
+
+@router.post('/upload_file')
+async def add_file_with_metadate(upload_file: UploadFile):  # UploadFile загружает метаинформацию вместе с самим содержимым
+    return {'File was processed. File name:': upload_file.filename}
